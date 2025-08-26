faecbafe6e1127e63f69f3d83ea4f120c7ef85a4
FreyreCorona <einierfreyre60@gmail.com>
2025-08-26 13:50:27 -0300

    improve error handling on database adapter

--- src/shortener_svc/internal/infrastructure/db/postgres/postgresql.go
+++ src/shortener_svc/internal/infrastructure/db/postgres/postgresql.go
@@ -4,6 +4,7 @@ package db
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/FreyreCorona/Shortly/src/shortener_svc/internal/domain"
@@ -36,11 +37,16 @@ func (r *PostgresURLRepository) Persist(url domain.URL) (domain.URL, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	tx, err := r.DB.Begin()
+	tx, err := r.DB.BeginTx(ctx, nil)
 	if err != nil {
 		return domain.URL{}, nil
 	}
-	defer tx.Rollback()
+	defer func() {
+		err := tx.Rollback()
+		if err != nil && !errors.Is(err, sql.ErrTxDone) {
+			_ = err // intencionally ignore the error
+		}
+	}()
 
 	query := New(r.DB).WithTx(tx)
 
@@ -51,7 +57,10 @@ func (r *PostgresURLRepository) Persist(url domain.URL) (domain.URL, error) {
 	if err != nil {
 		return domain.URL{}, nil
 	}
-	tx.Commit()
+	err = tx.Commit()
+	if err != nil {
+		return domain.URL{}, nil
+	}
 	return domain.URL{ID: u.ID, RawURL: url.RawURL, ShortCode: url.ShortCode, CreatedAt: u.CreatedAt.Time}, nil
 }
 
@@ -60,11 +69,16 @@ func (r *PostgresURLRepository) GetByShortCode(code string) (domain.URL, error) 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	tx, err := r.DB.Begin()
+	tx, err := r.DB.BeginTx(ctx, nil)
 	if err != nil {
 		return domain.URL{}, nil
 	}
-	defer tx.Rollback()
+	defer func() {
+		err := tx.Rollback()
+		if err != nil && !errors.Is(err, sql.ErrTxDone) {
+			_ = err // intencionally ignore the error
+		}
+	}()
 
 	query := New(r.DB).WithTx(tx)
 
@@ -72,6 +86,9 @@ func (r *PostgresURLRepository) GetByShortCode(code string) (domain.URL, error) 
 	if err != nil {
 		return domain.URL{}, err
 	}
-	tx.Commit()
+	err = tx.Commit()
+	if err != nil {
+		return domain.URL{}, nil
+	}
 	return domain.URL{ID: url.ID, RawURL: url.OriginalUrl, ShortCode: url.ShortCode, CreatedAt: url.CreatedAt.Time}, nil
 }
