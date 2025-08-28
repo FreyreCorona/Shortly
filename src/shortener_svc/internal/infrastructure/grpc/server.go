8a56703dfe3bab410f60f0f488ce7e7aafaaf370
FreyreCorona <einierfreyre60@gmail.com>
2025-08-28 07:55:29 -0300

    implement getURLByShortCode function on grpcServer

--- src/shortener_svc/internal/infrastructure/grpc/server.go
+++ src/shortener_svc/internal/infrastructure/grpc/server.go
@@ -2,8 +2,14 @@
 package grpc
 
 import (
+	"context"
+	"errors"
+
 	"github.com/FreyreCorona/Shortly/protos"
 	"github.com/FreyreCorona/Shortly/src/shortener_svc/internal/application"
+	"github.com/FreyreCorona/Shortly/src/shortener_svc/internal/domain"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Server struct {
@@ -15,7 +21,15 @@ func NewGRPCServer(service application.RetrieveURLService) *Server {
 	return &Server{Service: service}
 }
 
-func (s *Server) RetrieveURL(code string) error {
-	// TODO: implement
-	return nil
+func (s *Server) GetURLByShortCode(ctx context.Context, r *protos.GetURLRequest) (*protos.GetURLResponse, error) {
+	url, err := s.Service.GetURL(r.GetCode())
+	if err != nil {
+		switch {
+		case errors.Is(err, domain.ErrCodeEmpty):
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		default:
+			return nil, status.Error(codes.NotFound, "the requested url doesnt exist")
+		}
+	}
+	return &protos.GetURLResponse{RawUrl: url.RawURL, Code: url.ShortCode}, nil
 }
