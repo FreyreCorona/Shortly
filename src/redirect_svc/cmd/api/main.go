4efc5736408c76bc60adcc61b74f0bf263d667e4
FreyreCorona <einierfreyre60@gmail.com>
2025-08-28 13:18:58 -0300

    feat(redirect): implement gRPC server

--- src/redirect_svc/cmd/api/main.go
+++ src/redirect_svc/cmd/api/main.go
@@ -5,35 +5,52 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"sync"
 
 	"github.com/FreyreCorona/Shortly/src/redirect_svc/internal/application"
+	"github.com/FreyreCorona/Shortly/src/redirect_svc/internal/domain"
 	"github.com/FreyreCorona/Shortly/src/redirect_svc/internal/infrastructure/cache/valkey"
+	"github.com/FreyreCorona/Shortly/src/redirect_svc/internal/infrastructure/grpc"
 	httpAdapter "github.com/FreyreCorona/Shortly/src/redirect_svc/internal/infrastructure/http"
 )
 
 func main() {
 	// Valkey adapter
-	cache, err := valkey.NewValkeyCache(fmt.Sprintf("%s:%s", os.Getenv("VALKEY_ADDR"), os.Getenv("VALKEY_PORT")),
+	cache, err := valkey.NewValkeyCache(fmt.Sprintf("%s:%s", os.Getenv("VALKEY_HOST"), os.Getenv("VALKEY_PORT")),
 
 		os.Getenv("VALKEY_USERNAME"),
 		os.Getenv("VALKEY_PASSWORD"))
 	if err != nil {
 		log.Fatalf("cache connection error :%s", err.Error())
 	}
+
+	repo, err := grpc.NewGRPCRepository(fmt.Sprintf("%s:%s", os.Getenv("URL_SHORTENER_SVC_HOST"), os.Getenv("URL_SHORTENER_SVC_GRPC_PORT")))
+	if err != nil {
+		log.Fatalf("grpc client error :%v", err.Error())
+	}
+
+	var wg sync.WaitGroup
+
 	// stablish the adapter in the service
-	// TODO: REFERENCE THE REPO OBJECT FOR NewRedirectionService PARAMETER
+	wg.Go(func() {
+		if err := StartHTTPHandler(cache, repo); err != nil {
+			log.Printf("error on http handler :%v", err)
+		}
+	})
+
+	wg.Wait()
+}
+
+func StartHTTPHandler(cache domain.URLCacheRepository, repo domain.URLRepository) error {
 	service := application.NewRedirectionService(cache, nil)
 	handler := httpAdapter.NewHandler(service)
 
 	mux := http.NewServeMux()
 	handler.Routes(mux)
 
-	// running the service
-	runningPort, err := strconv.Atoi(os.Getenv("REDIRECT_SVC_PORT"))
-	if err != nil {
-		log.Fatal("Uknown port")
-	}
-	fmt.Printf("Service running on : %d \n", runningPort)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", runningPort), mux))
+	port := ":" + os.Getenv("REDIRECT_SVC_PORT")
+
+	log.Printf("HTTP handler running on %s", port)
+
+	return http.ListenAndServe(port, mux)
 }
