a5721bcc03f2b65bb0052792c04d3d53b1679395
FreyreCorona <einierfreyre60@gmail.com>
2025-09-02 09:48:45 -0300

    separe responsibilities from use-cases

--- src/redirect_svc/internal/application/get_url.go
+++ src/redirect_svc/internal/application/get_url.go
@@ -7,16 +7,18 @@ import (
 	"github.com/FreyreCorona/Shortly/src/redirect_svc/internal/domain"
 )
 
-type RedirectionService struct {
-	cache domain.URLCacheRepository
-	repo  domain.URLRepository
+type GetURLService struct {
+	cache   domain.URLCacheRepository
+	repo    domain.URLRepository
+	service *SetURL
 }
 
-func NewRedirectionService(cache domain.URLCacheRepository, repo domain.URLRepository) *RedirectionService {
-	return &RedirectionService{cache: cache, repo: repo}
+func NewRedirectionService(cache domain.URLCacheRepository, repo domain.URLRepository) *GetURLService {
+	s := NewSetURLService(cache)
+	return &GetURLService{cache: cache, repo: repo, service: s}
 }
 
-func (s *RedirectionService) GetURL(code string) (*domain.URL, error) {
+func (s *GetURLService) GetURL(code string) (*domain.URL, error) {
 	cached, err := s.cache.Get(code)
 	if err != nil && !errors.Is(err, domain.ErrNoCachedURL) {
 		return nil, err // unespected
@@ -31,7 +33,7 @@ func (s *RedirectionService) GetURL(code string) (*domain.URL, error) {
 		return nil, err
 	}
 
-	err = s.cache.Set(url)
+	err = s.service.SetURL(url)
 	if err != nil {
 		return nil, err
 	}
